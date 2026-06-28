91d9fa3059d636dedad7f47b6e5412891d2b3ae1
Kamit6337 <kamit6337@gmail.com>
2026-06-28 19:32:44 +0530

    minor

--- src/plugins/clerk.ts
+++ src/plugins/clerk.ts
@@ -11,6 +11,7 @@ export default fp(async (fastify) => {
     console.log("CLERK_PUBLISHABLE_KEY", environment.CLERK_PUBLISHABLE_KEY);
     console.log("CLERK_SECRET_KEY", environment.CLERK_SECRET_KEY);
     console.log("COOKIES", request.headers);
+    console.log("USER", user);
 
     if (!user.isAuthenticated) {
       return reply.code(401).send({
