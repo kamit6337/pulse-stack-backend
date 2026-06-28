69db41f233352aa12d673028efe662d63c0a58e1
Kamit6337 <kamit6337@gmail.com>
2026-06-28 19:19:52 +0530

    add console.log in clerk.ts

--- src/plugins/clerk.ts
+++ src/plugins/clerk.ts
@@ -1,11 +1,16 @@
 import fp from "fastify-plugin";
 import { clerkPlugin, getAuth } from "@clerk/fastify";
+import environment from "@/config/environment.js";
 
 export default fp(async (fastify) => {
   fastify.register(clerkPlugin);
 
   fastify.decorate("authenticateClerk", async (request, reply) => {
     const user = getAuth(request);
+
+    console.log("CLERK_PUBLISHABLE_KEY", environment.CLERK_PUBLISHABLE_KEY);
+    console.log("CLERK_SECRET_KEY", environment.CLERK_SECRET_KEY);
+    console.log("COOKIES", request.headers.cookie);
 
     if (!user.isAuthenticated) {
       return reply.code(401).send({
